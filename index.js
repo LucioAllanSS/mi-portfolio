a86ee90f8e44d3a285049661f0e9a9000367935b
Lucio Allan <104949600+LucioAllanSS@users.noreply.github.com>
2023-05-22 20:56:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var linkStyles = document.querySelector('link[href="Mobile.css"]');
         
     if (platform.includes('Win') || platform.includes('Linux') || platform.includes('Mac')){
-        linkStyles.href = "PC.css";
+        linkStyles.href = "/PC.css";
     }
         
     if (userAgent.includes('Android') || userAgent.includes('iPhone') || userAgent.includes('iPad') || userAgent.includes('iPod')) {
-        linkStyles.href = "PC.css";
+        linkStyles.href = "/PC.css";
     }
 });
