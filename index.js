f023826063f434308d26b53269c50c17079b7f6c
Lucio Allan <104949600+LucioAllanSS@users.noreply.github.com>
2023-05-22 20:42:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var userAgent = window.navigator.userAgent;
+var platform = window.navigator.platform;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var linkStyles = document.querySelector('link[href="Mobile.css"]');
+        
+    if (platform.includes('Win') || platform.includes('Linux') || platform.includes('Mac')){
+        linkStyles.href = "PC.css";
+    }
+        
+    if (userAgent.includes('Android') || userAgent.includes('iPhone') || userAgent.includes('iPad') || userAgent.includes('iPod')) {
+        linkStyles.href = "PC.css";
+    }
+});
