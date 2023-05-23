929cc8ebd5ca65b0924d719cb7e2d7b076fc230d
Lucio Allan <104949600+LucioAllanSS@users.noreply.github.com>
2023-05-22 23:15:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,4 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (userAgent.includes('Android') || userAgent.includes('iPhone') || userAgent.includes('iPad') || userAgent.includes('iPod')) {
         linkStyles.href = "PC.css";
     }
+
+
+document.getElementById("l").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/lucioallanss/"; // Reemplaza esta URL por la que deseas utilizar
+  });
+
+document.getElementById("g").addEventListener("click", function() {
+    window.location.href = "https://github.com/LucioAllanSS"; // Reemplaza esta URL por la que deseas utilizar
+  });
+
 });
